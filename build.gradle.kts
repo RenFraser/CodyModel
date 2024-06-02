d98f805679b7b41e812bf22c050faab96c818265
Henry Fraser <henrymfraser@gmail.com>
2024-06-03 07:13:47 +1000

    build: fix build
    The smithy* dependencies are redundant.
    I've installed smithy on my cli via brew and the build is done by gradle, so I don't need these.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,10 @@ repositories {
 }
 
 dependencies {
-    smithyCli("software.amazon.smithy:smithy-cli:1.49.0")
-    smithyBuild("software.amazon.smithy.typescript:smithy-aws-typescript-codegen:0.20.1")
-
+    implementation("software.amazon.smithy.typescript:smithy-typescript-codegen:0.21.0")
     implementation("software.amazon.smithy:smithy-model:1.49.0")
     implementation("software.amazon.smithy:smithy-linters:1.49.0")
     implementation("software.amazon.smithy:smithy-aws-traits:1.49.0")
-
 }
 
 // TODO: ensure I run testSmithyBuild somewhere - it's what validates stuff
